6e5ed8629794c3f45b17b5b179c4a237754e7b99
sander <san.d.erjonkers+github@gmail.com>
2023-11-21 18:04:43 +0100

    my_iperf3: iperf3_downstream_speed()

--- my_iperf3.py
+++ my_iperf3.py
@@ -4,15 +4,18 @@
 # Only works on Linux, with iperf3 binary & python module installed
 
 import iperf3 # needs iperf3 binary and iperf python module.
+def iperf3_downstream_speed():
+    client = iperf3.Client()
+    client.duration = 3 # seconds
+    client.num_streams = 20
+    client.server_hostname = 'ams.speedtest.clouvider.net'
+    client.port = 5206
+    client.reverse=True # Downstream
+    result = client.run() # run the test
+    # ... after some time:
+    Mbps = int(result.received_Mbps)
+    return Mbps
 
-print("Let's go ...")
-client = iperf3.Client()
-client.duration = 3 # seconds
-client.num_streams = 20
-client.server_hostname = 'ams.speedtest.clouvider.net'
-client.port = 5206
-client.reverse=True # Downstream
-result = client.run() # run the test
-
-print("Downstream speed [Mbps]", int(result.received_Mbps))
-
+if __name__ == "__main__":
+    print("Let's go ...")
+    print("Downstream speed [Mbps]", iperf3_downstream_speed())
