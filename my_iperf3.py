819578fce74126d704acfe63390459ac6faa8082
sander <san.d.erjonkers+github@gmail.com>
2023-11-22 18:58:25 +0100

    my_iperf3: more servers

--- my_iperf3.py
+++ my_iperf3.py
@@ -4,18 +4,44 @@
 # Only works on Linux, with iperf3 binary & python module installed
 
 import iperf3 # needs iperf3 binary and iperf python module.
-def iperf3_downstream_speed():
-    client = iperf3.Client()
-    client.duration = 3 # seconds
+
+'''
+"nyc.speedtest.clouvider.net"
+"la.speedtest.clouvider.net"
+"ams.speedtest.clouvider.net",
+"la.speedtest.clouvider.net",
+"fra.speedtest.clouvider.net",
+"ash.speedtest.clouvider.net",
+"nyc.speedtest.clouvider.net",
+"speedtest.uztelecom.uz",
+"proof.ovh.net",
+# "iperf.worldstream.nl",
+
+'''
+
+def iperf3_downstream_speed(server='ams.speedtest.clouvider.net', duration=3):
+    try:
+        client = iperf3.Client()
+    except:
+        return None
+    client.duration = duration # seconds
     client.num_streams = 20
-    client.server_hostname = 'ams.speedtest.clouvider.net'
+    client.server_hostname = server
     client.port = 5206
     client.reverse=True # Downstream
-    result = client.run() # run the test
-    # ... after some time:
-    Mbps = int(result.received_Mbps)
-    return Mbps
+    try:
+        result = client.run() # run the test
+        # ... after some time:
+        Mbps = int(result.received_Mbps)
+        return Mbps
+    except:
+        return None
 
 if __name__ == "__main__":
     print("Let's go ...")
+    print("Downstream speed [Mbps]", iperf3_downstream_speed("ams.speedtest.clouvider.net", 1))
+    print("Downstream speed [Mbps]", iperf3_downstream_speed("fra.speedtest.clouvider.net", 1))
+    print("Downstream speed [Mbps]", iperf3_downstream_speed("nyc.speedtest.clouvider.net", 1))
+    print("Downstream speed [Mbps]", iperf3_downstream_speed("la.speedtest.clouvider.net", 1))
+
     print("Downstream speed [Mbps]", iperf3_downstream_speed())
--- pick_random_item.py
+++ pick_random_item.py
@@ -1,7 +1,33 @@
 import random
 
 movie_list = ['The Godfather', 'The Wizard of Oz', 'Citizen Kane', 'The Shawshank Redemption', 'Pulp Fiction']
+movie_list = [*range(5200,5209+1)] # argument-unpacking operator
+movie_list = list(range(5200,5209+1))
+
+print(movie_list)
 
 # pick a random element from a list of strings.
 movie = random.choice(movie_list)
 print(movie)
+movie_list.remove(movie)
+
+movie = random.choice(movie_list)
+print(movie)
+movie_list.remove(movie)
+
+movie = random.choice(movie_list)
+print(movie)
+movie_list.remove(movie)
+
+movie = random.choice(movie_list)
+print(movie)
+movie_list.remove(movie)
+
+movie = random.choice(movie_list)
+print(movie)
+movie_list.remove(movie)
+
+movie = random.choice(movie_list)
+print(movie)
+movie_list.remove(movie)
+
