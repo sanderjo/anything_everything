98615cd6e9f14ff6b8dc8a89452ea10d2e6d30d9
sander <san.d.erjonkers+github@gmail.com>
2023-12-09 20:14:53 +0100

    Motto: reverse engineering is also engineering!

--- mkv_subtitles.py
+++ mkv_subtitles.py
@@ -3,6 +3,8 @@
 '''
 Finding subtitles in .MKV without any external tools
 Based on brute-force reverse engineering of some MKV files with head, hd, grep
+
+Motto: reverse engineering is also engineering!
 '''
 
 import sys
@@ -27,24 +29,25 @@ print(inputmkvfile)
 
 with open(inputmkvfile, 'rb') as f:
     s = f.read(20000) # MKV subtitle info is in the first part of the MKV, so only read that
-    for i in range(100): # max 100 subtitles
-        pos = s.find(searchstring,startpos)
-        if pos < 0:
-            # no (more) subtitle info found, so quit the for loop
-            break
 
-        # OK. Now search before and after that:
-        longerstring = s[pos-20:pos+20]
-        pospreambleforsublanguage = longerstring.find(preambleforsublanguage)
-        if pospreambleforsublanguage >=0 :
-            try:
-                sublanguage = longerstring[pospreambleforsublanguage+3:pospreambleforsublanguage+6].decode('utf-8')
-                if all(x.islower() for x in sublanguage):
-                    # all lowercase letters, so that certainly looks like a subtitle language!
-                    subfounds[sublanguage] = True
-            except:
-                pass
-        startpos = pos + 10  # skip a bit further
+for i in range(100): # max 100 subtitles
+    pos = s.find(searchstring,startpos)
+    if pos < 0:
+        # no (more) subtitle info found, so quit the for loop
+        break
+
+    # OK. Now search before and after that:
+    longerstring = s[pos-20:pos+20]
+    pospreambleforsublanguage = longerstring.find(preambleforsublanguage)
+    if pospreambleforsublanguage >=0 :
+        try:
+            sublanguage = longerstring[pospreambleforsublanguage+3:pospreambleforsublanguage+6].decode('utf-8')
+            if all(x.islower() for x in sublanguage):
+                # all lowercase letters, so that certainly looks like a subtitle language!
+                subfounds[sublanguage] = True
+        except:
+            pass
+    startpos = pos + 10  # skip a bit further
 
 
 if not subfounds:
