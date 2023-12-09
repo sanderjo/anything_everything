29563310fa6f88ad90a9cd74634f24f236f08e3f
sander <san.d.erjonkers+github@gmail.com>
2023-12-09 14:20:58 +0100

    resistant to all kinds of rainy days

--- mkv_subtitles.py
+++ mkv_subtitles.py
@@ -2,14 +2,41 @@
 
 import os
 import json
+import sys
+
+try:
+    inputmkvfile = sys.argv[1]
+except:
+    inputmkvfile = "~/something.mkv"
+
+if not os.path.isfile(inputmkvfile):
+    sys.exit(1)
 
 
-cmd = "mkvmerge -J ~/something.mkv"
-json_output = os.popen(cmd).read()
-mkv_details = json.loads(json_output)
+print("Inspecting", inputmkvfile)
 
-tracks = mkv_details['tracks']
+try:
+    cmd = "mkvmerge -J " + inputmkvfile
+    json_output = os.popen(cmd).read()
+except:
+    sys.exit("error running mkvmerge")
 
+try:
+    mkv_details = json.loads(json_output)
+    tracks = mkv_details['tracks']
+except:
+    sys.exit("error trying to get JSON tracks")
+
+subfounds ={}
 for i in tracks:
     if i["type"] == "subtitles":
-        print(i["properties"]["language"])
+        sub = i["properties"]["language"]
+        subfounds[sub] = 1
+
+#print(subfounds)
+if not subfounds:
+    print("no subs found")
+else:
+    for sub in subfounds:
+        print(sub, end=" ")
+    print("\n")
