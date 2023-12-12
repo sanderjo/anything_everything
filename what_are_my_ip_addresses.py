ab8087b8336a02102c3655590b2d0b1dbe0ef47c
sander <san.d.erjonkers+github@gmail.com>
2023-12-12 11:35:07 +0100

    ip address: with question mark

--- what_are_my_ip_addresses.py
+++ what_are_my_ip_addresses.py
@@ -7,23 +7,26 @@ Find public IPv4 and IPv6 of this client
 
 import requests # needed, as it works with cloudflare
 import socket
+import sys
 
 testhost = "self-test.sabnzbd.org"
 
-#  easy request:
-r = requests.get("https://" + testhost)
-print(r.content)
 
+#  easy request ... but does not specify ipv4 or ipv6:
+r = requests.get("https://" + testhost)
+print(r.content.decode('utf-8'))
 
 # connect via IPv4 address
 testhostipv4 = socket.getaddrinfo(testhost, 443, family=socket.AF_INET, proto=socket.IPPROTO_TCP)[0][4][0] # First ipv4 address of testhost
-r = requests.get(f"http://{testhostipv4}/#ipv4" , headers={'host': testhost})
+r = requests.get(f"http://{testhostipv4}/?ipv4", headers={'host': testhost})
 print(r.content.decode('utf-8'))
 
 # connect via IPv6 address
 testhostipv6 = socket.getaddrinfo(testhost, 443, family=socket.AF_INET6, proto=socket.IPPROTO_TCP)[0][4][0] # First ipv6 address of testhost
-r = requests.get(f"http://[{testhostipv6}]/#ipv6" , headers={'host': testhost})
+r = requests.get(f"http://[{testhostipv6}]/#?ipv6", headers={'host': testhost})
 print(r.content.decode('utf-8'))
+
+sys.exit(0)
 
 from urllib.request import urlopen
 url = "https://jsonplaceholder.typicode.com/todos/1"
