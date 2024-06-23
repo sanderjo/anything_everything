08d73dde6b364798cb8b015a2d8de32b0e63b138
sander <san.d.erjonkers+github@gmail.com>
2024-06-23 13:52:55 +0200

    SABnzbd intermediate script: mkv_checker

---
+++ SABnzbd_post_pre_intermediate_scripts/mkv_checker.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+# mkv checker, usable as SABnzbd intermediate script
+
+import os
+import sys
+
+'''
+SABnzbd specifies https://sabnzbd.org/wiki/configuration/4.3/scripts/pre-queue-scripts output priority
+
+Priority
+-100 = Default
+-2 = Paused
+-1 = Low
+0 = Normal
+1 = High
+2 = Force
+'''
+
+
+def check_subtitles(mkvfile):
+    # ugly way to test if there are subtitles in the mkv
+    cmd = f"mkvmerge -J {mkvfile} | grep -i subtitles"
+    if os.popen(cmd).readline().find('subtitle') >= 0:
+        return True
+    else:
+        return False
+
+
+#### MAIN ####
+
+try:
+    directory = sys.argv[1]
+except:
+    print("Error: specify directory")
+    sys.exit(-1)
+
+largest_file = max(
+    (os.path.join(root, file) for root, dirs, files in os.walk(directory) for file in files),
+    key=os.path.getsize
+)
+
+prio = 0  # default
+if largest_file.lower().endswith(".mkv"):
+    # yes, mkv
+    if check_subtitles(largest_file):
+        comment = "MKV, with subs"
+        prio = 1
+    else:
+        comment = "MKV, no subs"
+        prio = -1  # or 0 ...
+elif largest_file.lower().endswith(".bin"):
+    # my testfile
+    comment = "a bin, so maybe the testfile"
+    prio = 1
+else:
+    comment = "Nothing special"
+
+print(prio)
+print(largest_file)
+print(comment)
+
